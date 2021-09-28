0e10c1ca2ec80006e7ddf392a11e9c9563e170d0
Light <VerodiciaTheNatureGoddess@gmail.com>
2021-09-28 08:17:45 +0330

    Answered 1094

---
+++ 1094/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    uint32_t count = 0u;
+    uint64_t in = 0u;
+    uint64_t in_prev = 0u;
+    uint64_t steps = 0u;
+
+    std::cin >> count;
+    for(uint32_t i = 0u; i < count; i++)
+    {
+        std::cin >> in;
+        
+        if(in < in_prev)
+            steps += in_prev - in;
+        else
+            in_prev = in;
+    }
+
+    std::cout << steps;
+}
